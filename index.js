c0dfdc8efca907c7661a822101a4cab8db72234d
Philipp Czarnetzki <czarnetzki.philipp@protonmail.ch>
2017-12-23 23:07:29 +0100

    Removed obsolete wscript; Use NODE_GYP_MODULE_NAME; Exporting the binding

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-
-let ife = require('./build/Debug/IFEBinding').IFE
-
-let a = new ife();
-
-let success = a.up({ name: 'enp10s0', ip: '192.168.178.39', broadcast: '192.168.178.255', netmask: '255.255.255.0', network: '192.168.178.0' })
-
+module.exports = require('bindings')('IFEBinding')
